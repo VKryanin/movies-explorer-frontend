d8b6c40419c00e1c97973b51c4c6591c1e50dfe7
Vitaly Kryanin <krianinvv@internet.ru>
2023-08-11 16:33:34 +0300

    fix, 320

--- src/components/Navigation/NavPromo/NavPromo.jsx
+++ src/components/Navigation/NavPromo/NavPromo.jsx
@@ -6,10 +6,10 @@ export function NavPromo() {
     return (
         <nav className='header__navigation'>
             <ul className='header__list'>
-                <li className='header__item'>
+                <li className='header__li'>
                     <Link className='header__link header__link_type_register' to='/signup'>Регистрация</Link>
                 </li>
-                <li className='header__item'>
+                <li className='header__li'>
                     <Link className='header__link header__link_type_login' to='/signin'>Войти</Link>
                 </li>
             </ul>
